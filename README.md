2e5e25476d5ad04b7eee6b2247346f668f11eaa2
Kelvin Eduardo <kelvin@nibbletecnologia.com.br>
2021-12-07 12:33:26 -0300

    db stage

--- README.md
+++ README.md
@@ -15,3 +15,5 @@ Pacotes adicionais:
 
 
 // OBS: functions with next provider have some changes recently, the communication on auth with gh provider will be afflicted.(nextauth api)
+
+7 - yarn add faunadb
--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,16 +1,54 @@
 /* faltando a definição de scopo na função auth*/
+import { query as q } from 'faunadb'
 
 import NextAuth from "next-auth"
-import GithubProvider from "next-auth/providers/github"
+import GitHub from 'next-auth/providers/github'
+
+import { fauna } from '../../../services/fauna'
+
 
 
 export default NextAuth({
+  secret: process.env.SECRET,
   providers: [
-    GithubProvider({
+    GitHub({
       clientId: process.env.GITHUB_ID,
       clientSecret: process.env.GITHUB_SECRET,
-      
-    })
+    }),
   ],
+  callbacks: {
+    async signIn({ user, account, profile }) {
+      const { email } = user
+    
+      try { 
+        await fauna.query(
+          q.If(
+            q.Not(
+              q.Exists(
+                q.Match(
+                  q.Index('user_by_email'),
+                  q.Casefold(user.email)
+                )
+              )
+            ),
+            q.Create(
+              q.Collection('users'),
+              { data: { email } }
+            ),
+            q.Get(
+              q.Match(
+                q.Index('user_by_email'),
+                q.Casefold(user.email)
+              )
+            )
+          )
+        )
+
+       return true
+     } catch {
+       return false
+     }
+    },
+  }
 }) 
 
